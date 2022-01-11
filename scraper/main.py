e1f68775a33d7b3f7d1db86fa8e25d4015bf6ff0
jmblake <jmblake96@gmail.com>
2022-01-11 00:16:53 +0000

    Add writing to mongo

--- scraper/main.py
+++ scraper/main.py
@@ -4,8 +4,17 @@ restaurants.
 """
 from dataclasses import dataclass
 
+import pymongo
 import requests
 from lxml import html
+
+import config
+
+
+award_mapping = {"m": "ONE_STAR",
+                 "n": "TWO_STARS",
+                 "o": "THREE_STARS",
+                 "=": "BIB_GOURMAND"}
 
 
 @dataclass
@@ -20,6 +29,17 @@ class Restaurant:
 
     @staticmethod
     def from_restaurant_page_items(elem: html.HtmlElement) -> 'Restaurant':
+        # TODO: Tidy up this error handling
+        try:
+            award = award_mapping.get(elem
+                                      .find_class("card__menu-content")[0]
+                                      .find("div")
+                                      .find("i")
+                                      .text
+                                      .strip(), "")
+        except AttributeError:
+            award = ""
+
         restaurant = Restaurant(
             name=(elem
                   .find_class("card__menu-content")[0]
@@ -27,12 +47,7 @@ class Restaurant:
                   .find("a")
                   .text
                   .strip()),
-            award=award_mapping.get(elem
-                                    .find_class("card__menu-content")[0]
-                                    .find("div")
-                                    .find("i")
-                                    .text
-                                    .strip(), ""),
+            award=award,
             lat=elem.get("data-lat"),
             lng=elem.get("data-lng"),
             data_id=elem.get("data-id"),
@@ -50,19 +65,22 @@ class Restaurant:
         )
         return restaurant
 
+# TODO: Make purpose of filters/location more explicit
+# filters = [
+#     "3-stars-michelin",
+#     "2-stars-michelin",
+#     "1-star-michelin",
+#     "bib-gourmand",
+# ]
 
-filters = [
-    "3-stars-michelin",
-    "2-stars-michelin",
-    "1-star-michelin",
-    "bib-gourmand",
-]
+# location = [
+#     "gb",
+#     "greater-london",
+#     "london",
+# ]
 
-location = [
-    "gb",
-    "greater-london",
-    "london",
-]
+location = []
+filters = []
 
 url_base = "https://guide.michelin.com"
 location_string = "/".join(location)
@@ -86,12 +104,6 @@ def get_number_of_pages(url_with_filter: str) -> int:
     return num_pages
 
 
-award_mapping = {"m": "ONE_STAR",
-                 "n": "TWO_STARS",
-                 "o": "THREE_STARS",
-                 "=": "BIB_GOURMAND"}
-
-
 def collect_restaurant_elements(max_pages: int) -> list[html.HtmlElement]:
     restaurant_elements: list[html.HtmlElement] = []
     for i in range(1, max_pages + 1):
@@ -103,7 +115,24 @@ def collect_restaurant_elements(max_pages: int) -> list[html.HtmlElement]:
             .find_class("js-restaurant__list_item")
         )
         restaurant_elements.extend(restaurant_elements_on_page)
+        # TODO: Set up proper logging
+        if i % 10 == 0:
+            print(f"Collected {i}/{max_pages} pages")
     return restaurant_elements
+
+
+def write_to_mongodb(objs: list):
+    # TODO: Figure out flags to remove from the connection string
+    connection_string = (f"mongodb+srv://{config.user}:{config.password}"
+                         f"@{config.cluster}/{config.db_name}?"
+                         f"retryWrites=true&w=majority&ssl=true"
+                         f"&ssl_cert_reqs=CERT_NONE")
+
+    client = pymongo.MongoClient(connection_string,
+                                 serverSelectionTimeoutMS=2000)
+    db = client[config.db_name]
+    collection = db[config.collection_name]
+    collection.insert_many([obj.__dict__ for obj in objs])
 
 
 def main():
@@ -111,6 +140,7 @@ def main():
     restaurant_xml_elements = collect_restaurant_elements(max_pages)
     restaurants = [Restaurant.from_restaurant_page_items(item) for item in
                    restaurant_xml_elements]
+    write_to_mongodb(restaurants)
 
 
 if __name__ == '__main__':
